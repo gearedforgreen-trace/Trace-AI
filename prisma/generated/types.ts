add6328919bb12b505b67101abdf8154879683f8
Frantz Arty <artyfrantz@gmail.com>
2025-03-16 23:19:56 -0400

    Updated schema

--- prisma/generated/types.ts
+++ prisma/generated/types.ts
@@ -4,6 +4,16 @@ export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
   : ColumnType<T, T | undefined, T>;
 export type Timestamp = ColumnType<Date, Date | string, Date | string>;
 
+export const Status = {
+    ACTIVE: "ACTIVE",
+    INACTIVE: "INACTIVE"
+} as const;
+export type Status = (typeof Status)[keyof typeof Status];
+export const CouponType = {
+    FIXED: "FIXED",
+    PERCENTAGE: "PERCENTAGE"
+} as const;
+export type CouponType = (typeof CouponType)[keyof typeof CouponType];
 export type Account = {
     id: string;
     accountId: string;
@@ -19,6 +29,39 @@ export type Account = {
     createdAt: Timestamp;
     updatedAt: Timestamp;
 };
+export type Bin = {
+    id: string;
+    number: string;
+    typeOfMaterialId: string;
+    storeId: string;
+    description: string | null;
+    imageUrl: string;
+    status: Generated<Status>;
+    createdAt: Generated<Timestamp>;
+    updatedAt: Timestamp;
+    userId: string;
+    organizationId: string;
+    address1: string;
+    address2: string | null;
+    city: string;
+    state: string;
+    zip: string;
+    country: string;
+};
+export type Coupon = {
+    id: string;
+    name: string;
+    description: string | null;
+    imageUrl: string;
+    status: Generated<Status>;
+    couponType: CouponType;
+    couponCode: string;
+    pointsToRedeem: number;
+    createdAt: Generated<Timestamp>;
+    updatedAt: Timestamp;
+    userId: string;
+    organizationId: string;
+};
 export type Invitation = {
     id: string;
     organizationId: string;
@@ -27,6 +70,17 @@ export type Invitation = {
     status: string;
     expiresAt: Timestamp;
     inviterId: string;
+};
+export type Material = {
+    id: string;
+    name: string;
+    description: string | null;
+    imageUrl: string;
+    status: Generated<Status>;
+    createdAt: Generated<Timestamp>;
+    updatedAt: Timestamp;
+    userId: string;
+    organizationId: string;
 };
 export type Member = {
     id: string;
@@ -43,6 +97,14 @@ export type Organization = {
     createdAt: Timestamp;
     metadata: string | null;
 };
+export type Points = {
+    id: string;
+    userId: string;
+    points: number;
+    description: string | null;
+    createdAt: Generated<Timestamp>;
+    updatedAt: Timestamp;
+};
 export type Session = {
     id: string;
     expiresAt: Timestamp;
@@ -54,6 +116,17 @@ export type Session = {
     userId: string;
     impersonatedBy: string | null;
     activeOrganizationId: string | null;
+};
+export type Store = {
+    id: string;
+    name: string;
+    description: string | null;
+    imageUrl: string;
+    status: Generated<Status>;
+    createdAt: Generated<Timestamp>;
+    updatedAt: Timestamp;
+    userId: string;
+    organizationId: string;
 };
 export type Test = {
     id: Generated<number>;
@@ -70,6 +143,7 @@ export type User = {
     banned: boolean | null;
     banReason: string | null;
     banExpires: Timestamp | null;
+    currentPoints: Generated<number>;
 };
 export type Verification = {
     id: string;
@@ -81,10 +155,15 @@ export type Verification = {
 };
 export type DB = {
     account: Account;
+    Bin: Bin;
+    Coupon: Coupon;
     invitation: Invitation;
+    Material: Material;
     member: Member;
     organization: Organization;
+    Points: Points;
     session: Session;
+    Store: Store;
     Test: Test;
     user: User;
     verification: Verification;
