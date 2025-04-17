e73ca6f1d7af8cfb599368a995d7b5d17b281389
desyed <syedshihabdu@gmail.com>
2025-04-17 21:17:53 +0600

    signin

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: {
     position: "bottom-right"
-  },
+  },  
+
 };
 
 export default nextConfig;
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,19 +1,21 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
-import { admin,organization } from "better-auth/plugins"
-
+import { admin, organization, openAPI, bearer } from "better-auth/plugins"
 
 const prisma = new PrismaClient();
+
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
+        provider: "postgresql",
     }),
-    emailAndPassword: {  
-        enabled: true
+    emailAndPassword: {
+        enabled: true,
     },
     plugins: [
-        admin() ,
-        organization()
+        admin(),
+        organization(),
+        openAPI(),
+        bearer()
     ]
 });
